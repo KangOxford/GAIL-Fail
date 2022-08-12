fc1c10f138718ffc959ea711ecbec5d0226f56c5
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-12 14:18:15 +0000

    tianshou

---
+++ tianshou/main.py
@@ -0,0 +1,2 @@
+import gym, torch, numpy as np, torch.nn as nn
+import tianshou as ts
---
+++ tianshou/run_setup.sh
@@ -0,0 +1,29 @@
+sudo apt-get install -y \
+    libgl1-mesa-dev \
+    libgl1-mesa-glx \
+    libglew-dev \
+    libosmesa6-dev \
+    software-properties-common
+sudo apt-get install -y patchelf
+
+pip install opencv-python
+pip install PyHamcrest
+pip install protobuf
+pip install tensorflow
+pip install gym
+pip install free-mujoco-py
+pip install numpy
+pip install pyyaml
+pip install termcolor
+pip install json_tricks
+
+pip install ipython
+pip install tianshou
+
+git config --global user.email "kang.li@maths.ox.ac.uk"
+git config --global user.name "KangOxford"
+
+
+
+
+
