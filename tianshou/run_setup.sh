2bd10a49810eef5635e3295ef016d8f7f028efe4
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-12 15:43:10 +0000

    init

--- tianshou/run_setup.sh
+++ tianshou/run_setup.sh
@@ -6,18 +6,20 @@ sudo apt-get install -y \
     software-properties-common
 sudo apt-get install -y patchelf
 
-# pip install opencv-python
-pip install PyHamcrest
-pip install protobuf
-pip install tensorflow
-pip install gym
+pip install --upgrade pip
 pip install free-mujoco-py
-pip install numpy
-pip install pyyaml
-pip install termcolor
-pip install json_tricks
 
-pip install ipython
+# pip install opencv-python
+# pip install PyHamcrest
+# pip install protobuf
+# pip install tensorflow
+# pip install gym
+# pip install numpy
+# pip install pyyaml
+# pip install termcolor
+# pip install json_tricks
+
+# pip install ipython
 pip install tianshou
 
 git config --global user.email "kang.li@maths.ox.ac.uk"
