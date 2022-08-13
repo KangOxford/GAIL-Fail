bc4c0be0760a0eafc8738c541f880997193635a0
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-13 16:21:36 +0000

    init imitation

--- setup/conda_setup.sh
+++
@@ -1,9 +0,0 @@
-wget "https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh"
-
-bash Anaconda3-2019.03-Linux-x86_64.sh
-
-rm Anaconda3-2019.03-Linux-x86_64.sh
-
-export PATH=/workspace/anaconda3/bin:$PATH
-/workspace/anaconda3/bin/conda init init bash 
-conda create -n gail python=3.7.3
--- setup/conda_setup2.sh
+++
@@ -1,3 +0,0 @@
-conda activate gail
-pip install --upgrade pip setuptools wheel
-pip install --ignore-installed imageio==2.9.0
