474c97595030ff1668cf9bca266e15fb66aa4be5
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-18 18:29:54 +0100

    update

--- run_setup.sh
+++ run_setup.sh
@@ -1,57 +1,54 @@
 # =============================  run on mac  =============================
-brew install gcc
-brew install wget 
-# brew install build-essential 
-# brew install libosmesa6-dev 
-# brew install libglew-dev
+if ["$(uname)"=="Darwin"];then
+    brew install gcc
+    brew install wget 
+    # brew install build-essential 
+    # brew install libosmesa6-dev 
+    # brew install libglew-dev
 
-mkdir /Users/$USER/.mujoco 
-wget http://www.roboti.us/download/mjpro150_osx.zip
-unzip mjpro150_osx.zip -d /Users/$USER/.mujoco 
-rm mjpro150_osx.zip
+    mkdir /Users/$USER/.mujoco 
+    wget http://www.roboti.us/download/mjpro150_osx.zip
+    unzip mjpro150_osx.zip -d /Users/$USER/.mujoco 
+    rm mjpro150_osx.zip
 
-wget http://www.roboti.us/download/mujoco200_macos.zip
-unzip mujoco200_macos.zip -d /Users/$USER/.mujoco 
-rm mujoco200_macos.zip
+    wget http://www.roboti.us/download/mujoco200_macos.zip
+    unzip mujoco200_macos.zip -d /Users/$USER/.mujoco 
+    rm mujoco200_macos.zip
 
-wget http://www.roboti.us/file/mjkey.txt
-cp mjkey.txt /Users/$USER/.mujoco/mjkey.txt
-rm mjkey.txt
+    wget http://www.roboti.us/file/mjkey.txt
+    cp mjkey.txt /Users/$USER/.mujoco/mjkey.txt
+    rm mjkey.txt
 
-export PATH=/Users/$USER/.mujoco/mjpro150/bin:$PATH
-export PATH=/Users/$USER/.mujoco/mujoco200_macos/bin:$PATH
-export MUJOCO_PY_MUJOCO_PATH=/Users/$USER/.mujoco/mjpro150/bin:$MUJOCO_PY_MUJOCO_PATH
-export MUJOCO_PY_MUJOCO_PATH=/Users/$USER/.mujoco/mujoco200_macos/bin:$MUJOCO_PY_MUJOCO_PATH
+    export PATH=/Users/$USER/.mujoco/mjpro150/bin:$PATH
+    export PATH=/Users/$USER/.mujoco/mujoco200_macos/bin:$PATH
+    export MUJOCO_PY_MUJOCO_PATH=/Users/$USER/.mujoco/mjpro150/bin:$MUJOCO_PY_MUJOCO_PATH
+    export MUJOCO_PY_MUJOCO_PATH=/Users/$USER/.mujoco/mujoco200_macos/bin:$MUJOCO_PY_MUJOCO_PATH
+
 
 # ============================= run on colab =============================
-export PATH=/workspace/.mujoco/mjpro150/bin:$PATH
-export PATH=/workspace/.mujoco/mjpro200_linux/bin:$PATH
-export MUJOCO_PY_MUJOCO_PATH=/workspace/.mujoco/mjpro150/bin:$MUJOCO_PY_MUJOCO_PATH
-export MUJOCO_PY_MUJOCO_PATH=/workspace/.mujoco/mjpro200_linux/bin:$MUJOCO_PY_MUJOCO_PATH
-export PATH=/workspace/anaconda3/bin:$PATH
+elif ["$uname"=="Linux"];then
+    export PATH=/workspace/.mujoco/mjpro150/bin:$PATH
+    export PATH=/workspace/.mujoco/mjpro200_linux/bin:$PATH
+    export MUJOCO_PY_MUJOCO_PATH=/workspace/.mujoco/mjpro150/bin:$MUJOCO_PY_MUJOCO_PATH
+    export MUJOCO_PY_MUJOCO_PATH=/workspace/.mujoco/mjpro200_linux/bin:$MUJOCO_PY_MUJOCO_PATH
+    export PATH=/workspace/anaconda3/bin:$PATH
+    # export PATH=/workspace/GAIL-Fail/stable-baselines/d2-imitation:$PATH
+    # export PATH=/workspace/GAIL-Fail/stable-baselines/d2-imitation/baselines:$PATH
+    sudo apt-get update && sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
+    sudo apt-get install -y \
+        libgl1-mesa-dev \
+        libgl1-mesa-glx \
+        libglew-dev \
+        libosmesa6-dev \
+        software-properties-common \
+        patchelf
 
-# export PATH=/workspace/GAIL-Fail/stable-baselines/d2-imitation:$PATH
-# export PATH=/workspace/GAIL-Fail/stable-baselines/d2-imitation/baselines:$PATH
+    # conda init bash
+    # conda activate gail
+    # ipython
 
-
-sudo apt-get update && sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
-sudo apt-get install -y \
-    libgl1-mesa-dev \
-    libgl1-mesa-glx \
-    libglew-dev \
-    libosmesa6-dev \
-    software-properties-common \
-    patchelf
-
-conda init bash
-conda activate gail
-ipython
-
-
-# import os
-# os.sys.path.append("/workspace/GAIL-Fail/stable-baselines/d2-imitation")
-
-# pip install hrl-pybullet-envs ## not sure about whether it can make 'import pybullet_envs' work
-
-
-# git rm -r --cached .
+    # import os
+    # os.sys.path.append("/workspace/GAIL-Fail/stable-baselines/d2-imitation")
+    # pip install hrl-pybullet-envs ## not sure about whether it can make 'import pybullet_envs' work
+    # git rm -r --cached .
+fi
