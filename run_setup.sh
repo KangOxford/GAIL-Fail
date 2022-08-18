f372cc2a95db3fc13f80b3fb469b166414f7e92a
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-18 18:23:02 +0100

    update

--- run_setup.sh
+++ run_setup.sh
@@ -1,3 +1,29 @@
+# =============================  run on mac  =============================
+brew install gcc
+brew install wget 
+# brew install build-essential 
+# brew install libosmesa6-dev 
+# brew install libglew-dev
+
+mkdir /Users/$USER/.mujoco 
+wget http://www.roboti.us/download/mjpro150_osx.zip
+unzip mjpro150_osx.zip -d /Users/$USER/.mujoco 
+rm mjpro150_osx.zip
+
+wget http://www.roboti.us/download/mujoco200_macos.zip
+unzip mujoco200_macos.zip -d /Users/$USER/.mujoco 
+rm mujoco200_macos.zip
+
+wget http://www.roboti.us/file/mjkey.txt
+cp mjkey.txt /Users/$USER/.mujoco/mjkey.txt
+rm mjkey.txt
+
+export PATH=/Users/$USER/.mujoco/mjpro150/bin:$PATH
+export PATH=/Users/$USER/.mujoco/mujoco200_macos/bin:$PATH
+export MUJOCO_PY_MUJOCO_PATH=/Users/$USER/.mujoco/mjpro150/bin:$MUJOCO_PY_MUJOCO_PATH
+export MUJOCO_PY_MUJOCO_PATH=/Users/$USER/.mujoco/mujoco200_macos/bin:$MUJOCO_PY_MUJOCO_PATH
+
+# ============================= run on colab =============================
 export PATH=/workspace/.mujoco/mjpro150/bin:$PATH
 export PATH=/workspace/.mujoco/mjpro200_linux/bin:$PATH
 export MUJOCO_PY_MUJOCO_PATH=/workspace/.mujoco/mjpro150/bin:$MUJOCO_PY_MUJOCO_PATH
