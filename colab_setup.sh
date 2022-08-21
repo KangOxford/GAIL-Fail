6dd8a0b647c7d2ac0b034c0628db6999b72a4351
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-21 02:05:20 +0100

    add

--- colab_setup.sh
+++ colab_setup.sh
@@ -1,23 +1,23 @@
-# sudo apt-get update && sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
-# sudo apt-get install -y libgl1-mesa-dev libgl1-mesa-glx libgl
+sudo apt-get update && sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
+sudo apt-get install -y libgl1-mesa-dev libgl1-mesa-glx libgl
 
-# mkdir ~/.mujoco 
-# wget http://www.roboti.us/download/mjpro150_osx.zip
-# unzip mjpro150_osx.zip -d ~/.mujoco 
-# rm mjpro150_osx.zip
+mkdir ~/.mujoco 
+wget http://www.roboti.us/download/mjpro150_osx.zip
+unzip mjpro150_osx.zip -d ~/.mujoco 
+rm mjpro150_osx.zip
 
-# wget http://www.roboti.us/download/mujoco200_macos.zip
-# unzip mujoco200_macos.zip -d ~/.mujoco 
-# rm mujoco200_macos.zip
+wget http://www.roboti.us/download/mujoco200_macos.zip
+unzip mujoco200_macos.zip -d ~/.mujoco 
+rm mujoco200_macos.zip
 
-# wget http://www.roboti.us/file/mjkey.txt
-# cp mjkey.txt ~/.mujoco/mjkey.txt
-# rm mjkey.txt
+wget http://www.roboti.us/file/mjkey.txt
+cp mjkey.txt ~/.mujoco/mjkey.txt
+rm mjkey.txt
 
-# export PATH=~/.mujoco/mjpro150/bin:$PATH
-# export PATH=~/.mujoco/mjpro200_linux/bin:$PATH
-# export MUJOCO_PY_MUJOCO_PATH=~/.mujoco/mjpro150/bin:$MUJOCO_PY_MUJOCO_PATH
-# export MUJOCO_PY_MUJOCO_PATH=~/.mujoco/mjpro200_linux/bin:$MUJOCO_PY_MUJOCO_PATH
+export PATH=~/.mujoco/mjpro150/bin:$PATH
+export PATH=~/.mujoco/mjpro200_linux/bin:$PATH
+export MUJOCO_PY_MUJOCO_PATH=~/.mujoco/mjpro150/bin:$MUJOCO_PY_MUJOCO_PATH
+export MUJOCO_PY_MUJOCO_PATH=~/.mujoco/mjpro200_linux/bin:$MUJOCO_PY_MUJOCO_PATH
 
 pip install seals
 pip install stable-baselines3
